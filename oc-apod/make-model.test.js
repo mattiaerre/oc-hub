d358216afff2d5b869fe28998aad13fa5c8bfea5
mattia richetto <mrichetto@opentable.com>
2017-03-19 21:25:02 -0700

    add sample image

--- oc-apod/make-model.test.js
+++ oc-apod/make-model.test.js
@@ -1,7 +1,8 @@
 import makeModel from './make-model';
+import image from './sample.image.json';
 import video from './sample.video.json';
 
-const scenarios = [video]; // todo: add image sample
+const scenarios = [image, video];
 
 scenarios.forEach((scenario) => {
   test(`${scenario.media_type} to match snapshot`, () => {
