7157cb13b0889fd2b6d3ebe1d7e7347b9938f23a
mattia richetto <mattia.richetto@gmail.com>
2017-05-09 08:38:46 -0700

    move requirejs end of body

--- how-to-ssr/public/javascripts/main.js
+++ how-to-ssr/public/javascripts/main.js
@@ -1,9 +1,11 @@
 requirejs.config({
   paths: {
-    jquery: 'https://code.jquery.com/jquery-3.2.1.min'
+    jquery: 'https://code.jquery.com/jquery-3.2.1.min',
+    'chegg-oc-client': 'http://components-test-cdn.cheggnet.com/components/oc-client/0.36.27/src/oc-client.min'
   }
 });
 
 requirejs(['jquery', 'oc-client.min'], ($, ocClient) => {
   console.log('ocClient:', ocClient); // eslint-disable-line no-console
+  console.log('ocClient.renderedComponents:', ocClient.renderedComponents); // eslint-disable-line no-console
 });
