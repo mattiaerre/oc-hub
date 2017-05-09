bacd463c0faca56f8108fb436ad43e6ce6358d85
mattia richetto <mattia.richetto@gmail.com>
2017-05-08 19:24:57 -0700

    delete how-to-requirejs folder

--- how-to-requirejs/main.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-  paths: {
-    jquery: 'https://code.jquery.com/jquery-3.2.1.min'
-  }
-});
-
-requirejs(['jquery', 'oc-client.min'], ($, ocClient) => {
-  console.log('ocClient:', ocClient); // eslint-disable-line no-console
-});
