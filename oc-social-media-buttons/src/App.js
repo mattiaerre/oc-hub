954ce7490c1c96698c6e158daefa7d66e91d7870
mattia richetto <mattia.richetto@gmail.com>
2018-12-09 21:34:55 -0800

    no more instagram

--- oc-social-media-buttons/src/App.js
+++ oc-social-media-buttons/src/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const data = [
-  { className: 'icon-twitter', href: 'https://twitter.com/mattiaerre', text: '@mattiaerre' },
-  { className: 'icon-instagram', href: 'https://www.instagram.com/mattiaerre2012', text: '@mattiaerre2012' },
-  { className: 'icon-github', href: 'https://github.com/mattiaerre', text: 'mattiaerre' },
-  { className: 'icon-wordpress', href: 'https://mattiarichetto.wordpress.com', text: 'mattiarichetto' },
-  { className: 'icon-npm', href: 'https://www.npmjs.com/~mattiaerre', text: '@mattiaerre' },
-  { className: 'icon-medium', href: 'https://medium.com/@mattiaerre', text: '@mattiaerre' }
+  {
+    className: 'icon-twitter',
+    href: 'https://twitter.com/mattiaerre',
+    text: '@mattiaerre'
+  },
+  {
+    className: 'icon-github',
+    href: 'https://github.com/mattiaerre',
+    text: 'mattiaerre'
+  },
+  {
+    className: 'icon-wordpress',
+    href: 'https://mattiarichetto.wordpress.com',
+    text: 'mattiarichetto'
+  },
+  {
+    className: 'icon-npm',
+    href: 'https://www.npmjs.com/~mattiaerre',
+    text: '@mattiaerre'
+  },
+  {
+    className: 'icon-medium',
+    href: 'https://medium.com/@mattiaerre',
+    text: '@mattiaerre'
+  }
 ];
 
 const Ul = styled.ul`
@@ -24,15 +43,13 @@ const App = () => {
   const list = data.map(element => (
     <Li>
       <i className={element.className} />
-      <a href={element.href} rel="noopener noreferrer" target="_blank">{element.text}</a>
+      <a href={element.href} rel="noopener noreferrer" target="_blank">
+        {element.text}
+      </a>
     </Li>
   ));
 
-  return (
-    <Ul>
-      {list}
-    </Ul>
-  );
+  return <Ul>{list}</Ul>;
 };
 
 export default App;
